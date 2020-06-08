0d3bd78ba9b39bcb3898ef965b32b457f0dff637
Ravikanth Reddy <ravikantreddy97@gmail.com>
2020-06-07 19:06:35 -0700

    Updates

---
+++ precache-manifest.5cc748a3e6ec673f2742088b2c7faf78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a886f85f29a6d7990ce3bdece0378ec",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "8c4fb31cf27d83b3eb73",
+    "url": "/todo-app/static/css/main.0d50167e.chunk.css"
+  },
+  {
+    "revision": "0897e83c2e52b2eca7eb",
+    "url": "/todo-app/static/js/2.412b4cb2.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/todo-app/static/js/2.412b4cb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c4fb31cf27d83b3eb73",
+    "url": "/todo-app/static/js/main.2368b049.chunk.js"
+  },
+  {
+    "revision": "a1fe2c5b964b43636895",
+    "url": "/todo-app/static/js/runtime-main.6020875f.js"
+  }
+]);
